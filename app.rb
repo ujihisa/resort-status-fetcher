00fde4423d91a5a1b7a28e0f0bc0dfa27fecb18f
ujihisa <ujihisa@users.noreply.github.com>
2021-01-16 09:20:32 -0800

    Trivial tweaks

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require 'sinatra'
 require 'nokogiri'
 require 'net/http'
 
-set :port, 3000
-set :bind, '0.0.0.0'
+set(:bind, '0.0.0.0')
+set(:port, ENV['PORT'] || '8080')
 
 get '/' do
   doc = Nokogiri::HTML(Net::HTTP.get(URI('https://www.grousemountain.com/current_conditions')))
