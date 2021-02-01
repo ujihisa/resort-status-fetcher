b4ba8d5998fb5be46d3c3090e565112c8e27d577
ujihisa <ujihisa@users.noreply.github.com>
2021-01-31 20:08:18 -0800

    Support both

--- show_differences.rb
+++ show_differences.rb
@@ -1,13 +1,33 @@
-# let b:quickrun_config = {'exec': 'doo bundle exec ruby show_differences.rb'}
+# let b:quickrun_config = {'exec': 'doo -b bundle exec ruby show_differences.rb'}
 require 'google/cloud/firestore'
 
-firestore = ::Google::Cloud::Firestore.new(
-  project_id: 'devs-sandbox',
-  credentials: 'devs-sandbox-5941dd8999bb.json')
+def show_differences
+  firestore = ::Google::Cloud::Firestore.new(
+    project_id: 'devs-sandbox',
+    credentials: 'devs-sandbox-5941dd8999bb.json')
 
-col = firestore.col('ujihisa-test')
-col.get.sort_by { _1[:time] }.map {|x|
-  [x[:time], eval(x[:grouse][:runs]).to_set]
-}.each_cons(2) {|(x_time, x_runs), (y_time, y_runs)|
-  pp [y_time, x_runs - y_runs, y_runs - x_runs] if x_runs != y_runs
-}
+  col = firestore.col('ujihisa-test')
+  col.get.sort_by { _1[:time] }.map {|x|
+    [x[:time], eval(x[:grouse][:runs]).to_set]
+  }.each_cons(2) {|(x_time, x_runs), (y_time, y_runs)|
+    pp [y_time, x_runs - y_runs, y_runs - x_runs] if x_runs != y_runs
+  }
+end
+
+def show_after(time)
+  firestore = ::Google::Cloud::Firestore.new(
+    project_id: 'devs-sandbox',
+    credentials: 'devs-sandbox-5941dd8999bb.json')
+
+  col = firestore.col('ujihisa-test')
+  after = col.get.sort_by { _1[:time] }.detect { time < _1[:time] }
+  p after[:time]
+  after[:grouse].each do |name, str|
+    puts '--------------------'
+    p name
+    pp JSON.parse(str)
+  end
+end
+
+# show_differences()
+show_after(Time.parse('2021-01-27T12:32:02-08:00'))
